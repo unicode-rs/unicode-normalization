6e2f5f0e445aead1c083a2ba71e7b1d0e564b7fb
Braden Walker <bw@dropbox.com>
2018-01-25 11:41:34 -0800

    Implement stream-safe text process (UAX15-D4)

--- src/test.rs
+++ src/test.rs
@@ -13,6 +13,14 @@ use std::char;
 use super::UnicodeNormalization;
 use super::char::is_combining_mark;
 
+fn to_codepoint_seq(s: String) -> String {
+    s.chars().map(|c| format!("U+{:04X} ", c as u32)).collect::<String>()
+}
+macro_rules! assert_eq_strs {
+    ($left: expr, $right: expr) => {
+        assert_eq!(to_codepoint_seq($left.to_string()), to_codepoint_seq($right.to_string()));
+    }
+}
 
 #[test]
 fn test_nfd() {
@@ -34,6 +42,91 @@ fn test_nfd() {
     t!("\u{301}a", "\u{301}a");
     t!("\u{d4db}", "\u{1111}\u{1171}\u{11b6}");
     t!("\u{ac1c}", "\u{1100}\u{1162}");
+    t!("\u{E1}\u{325}\u{E1}\u{325}", "a\u{325}\u{301}a\u{325}\u{301}");
+    t!("\u{E1}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}",
+        "a\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}");
+    t!("\u{E1}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}",
+        "a\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}");
+}
+
+#[test]
+fn test_nfd_stream_safe() {
+    macro_rules! t {
+        ($input: expr, $expected: expr) => {
+            assert_eq_strs!($input.nfd_stream_safe().to_string(), $expected);
+            // A dummy iterator that is not std::str::Chars directly;
+            // note that `id_func` is used to ensure `Clone` implementation
+            assert_eq_strs!($input.chars().map(|c| c).nfd_stream_safe().collect::<String>(), $expected);
+        }
+    }
+    t!("abc", "abc");
+    t!("\u{1e0b}\u{1c4}", "d\u{307}\u{1c4}");
+    t!("\u{2026}", "\u{2026}");
+    t!("\u{2126}", "\u{3a9}");
+    t!("\u{1e0b}\u{323}", "d\u{323}\u{307}");
+    t!("\u{1e0d}\u{307}", "d\u{323}\u{307}");
+    t!("a\u{301}", "a\u{301}");
+    t!("\u{301}a", "\u{301}a");
+    t!("\u{d4db}", "\u{1111}\u{1171}\u{11b6}");
+    t!("\u{ac1c}", "\u{1100}\u{1162}");
+    t!("a\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}",
+        "a\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{34f}\
+        \u{301}\u{301}");
+    t!("\u{E1}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}",
+        "a\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{34f}\
+        \u{301}\u{301}");
+    t!("\u{E1}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}",
+        "a\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{34f}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{34f}\
+        \u{301}\u{301}");
 }
 
 #[test]
@@ -59,7 +152,9 @@ fn test_nfkd() {
 fn test_nfc() {
     macro_rules! t {
         ($input: expr, $expected: expr) => {
-            assert_eq!($input.nfc().to_string(), $expected);
+            assert_eq_strs!($input.nfc().to_string(), $expected);
+            //assert_eq!($input.nfc().map(|c| format!("U+{:04X},", c as u32)).collect::<String>(), $expected.chars().map(|c| format!("U+{:04X},", c as u32)).collect::<String>())
+            //assert_eq!($input.nfc().to_string(), $expected);
         }
     }
     t!("abc", "abc");
@@ -73,6 +168,53 @@ fn test_nfc() {
     t!("\u{d4db}", "\u{d4db}");
     t!("\u{ac1c}", "\u{ac1c}");
     t!("a\u{300}\u{305}\u{315}\u{5ae}b", "\u{e0}\u{5ae}\u{305}\u{315}b");
+    // U+0325 will get sorted to the front because it's CCC=220, and U+0301 is CCC=230, then it'll
+    // canonically combine with the a to make U+1E01.
+    t!("a\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{325}",
+        "\u{1E01}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}");
+}
+
+#[test]
+fn test_nfc_stream_safe() {
+    macro_rules! t {
+        ($input: expr, $expected: expr) => {
+            //assert_eq!($input.nfc_stream_safe().to_string(), $expected);
+            assert_eq!($input.nfc_stream_safe().map(|c| format!("U+{:04X},", c as u32)).collect::<String>(), $expected.chars().map(|c| format!("U+{:04X},", c as u32)).collect::<String>())
+        }
+    }
+    t!("abc", "abc");
+    t!("\u{1e0b}\u{1c4}", "\u{1e0b}\u{1c4}");
+    t!("\u{2026}", "\u{2026}");
+    t!("\u{2126}", "\u{3a9}");
+    t!("\u{1e0b}\u{323}", "\u{1e0d}\u{307}");
+    t!("\u{1e0d}\u{307}", "\u{1e0d}\u{307}");
+    t!("a\u{301}", "\u{e1}");
+    t!("\u{301}a", "\u{301}a");
+    t!("\u{d4db}", "\u{d4db}");
+    t!("\u{ac1c}", "\u{ac1c}");
+    t!("a\u{300}\u{305}\u{315}\u{5ae}b", "\u{e0}\u{5ae}\u{305}\u{315}b");
+    // In stream safe mode, COMBINING GRAPHEME JOINER (U+034F) will get inserted after 30 combining
+    // characters, which breaks up the sort/recombine so the U+0325 will not get sorted next to the
+    // a, so it'll combine with a U+0301 to U+00E1 instead.
+    t!("a\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{325}",
+        "\u{E1}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\u{301}\
+        \u{34F}\
+        \u{325}\u{301}\u{301}");
 }
 
 #[test]
@@ -111,11 +253,11 @@ fn test_official() {
             let r3 = normString!(nfc, s3);
             let r4 = normString!(nfc, s4);
             let r5 = normString!(nfc, s5);
-            assert_eq!(s2, &r1[..]);
-            assert_eq!(s2, &r2[..]);
-            assert_eq!(s2, &r3[..]);
-            assert_eq!(s4, &r4[..]);
-            assert_eq!(s4, &r5[..]);
+            assert_eq_strs!(s2, &r1[..]);
+            assert_eq_strs!(s2, &r2[..]);
+            assert_eq_strs!(s2, &r3[..]);
+            assert_eq_strs!(s4, &r4[..]);
+            assert_eq_strs!(s4, &r5[..]);
         }
 
         {
@@ -124,11 +266,11 @@ fn test_official() {
             let r3 = normString!(nfd, s3);
             let r4 = normString!(nfd, s4);
             let r5 = normString!(nfd, s5);
-            assert_eq!(s3, &r1[..]);
-            assert_eq!(s3, &r2[..]);
-            assert_eq!(s3, &r3[..]);
-            assert_eq!(s5, &r4[..]);
-            assert_eq!(s5, &r5[..]);
+            assert_eq_strs!(s3, &r1[..]);
+            assert_eq_strs!(s3, &r2[..]);
+            assert_eq_strs!(s3, &r3[..]);
+            assert_eq_strs!(s5, &r4[..]);
+            assert_eq_strs!(s5, &r5[..]);
         }
 
         {
@@ -137,11 +279,11 @@ fn test_official() {
             let r3 = normString!(nfkc, s3);
             let r4 = normString!(nfkc, s4);
             let r5 = normString!(nfkc, s5);
-            assert_eq!(s4, &r1[..]);
-            assert_eq!(s4, &r2[..]);
-            assert_eq!(s4, &r3[..]);
-            assert_eq!(s4, &r4[..]);
-            assert_eq!(s4, &r5[..]);
+            assert_eq_strs!(s4, &r1[..]);
+            assert_eq_strs!(s4, &r2[..]);
+            assert_eq_strs!(s4, &r3[..]);
+            assert_eq_strs!(s4, &r4[..]);
+            assert_eq_strs!(s4, &r5[..]);
         }
 
         {
@@ -150,11 +292,11 @@ fn test_official() {
             let r3 = normString!(nfkd, s3);
             let r4 = normString!(nfkd, s4);
             let r5 = normString!(nfkd, s5);
-            assert_eq!(s5, &r1[..]);
-            assert_eq!(s5, &r2[..]);
-            assert_eq!(s5, &r3[..]);
-            assert_eq!(s5, &r4[..]);
-            assert_eq!(s5, &r5[..]);
+            assert_eq_strs!(s5, &r1[..]);
+            assert_eq_strs!(s5, &r2[..]);
+            assert_eq_strs!(s5, &r3[..]);
+            assert_eq_strs!(s5, &r4[..]);
+            assert_eq_strs!(s5, &r5[..]);
         }
     }
 }
